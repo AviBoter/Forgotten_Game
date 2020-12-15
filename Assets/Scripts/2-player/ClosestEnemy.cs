bc052354e46d5cb7f7f6307c773dacbf9d60f15c
AviBoter <maccavi3@gmail.com>
2020-12-15 02:12:56 +0200

    First Commit

---
+++ Assets/Scripts/2-player/ClosestEnemy.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+
+public class ClosestEnemy : MonoBehaviour
+{
+    private Transform Player;
+
+    public List<Transform> EnemyList;
+
+    public Transform nearestEnemy ;
+
+    [Tooltip("attack radius")]
+    [SerializeField] private float radius = 2f;
+
+
+    void Start()
+    {
+        Player = gameObject.GetComponent<Transform>();
+        StartCoroutine(waiter());
+    }
+
+   
+    IEnumerator waiter()
+    {
+        for (; ; )
+        {
+            float minimumDistance = Mathf.Infinity;
+
+            nearestEnemy = null;
+            foreach (Transform enemy in EnemyList)
+            {
+                if (enemy != null)
+                {
+                    float distance = Vector2.Distance(Player.position, enemy.position);
+                    if (distance < minimumDistance)
+                    {
+                        minimumDistance = distance;
+                        nearestEnemy = enemy;
+                    }
+                }
+
+
+            }
+            if (Vector2.Distance(transform.position, nearestEnemy.position) <= radius)
+            {
+                Destroy(nearestEnemy.gameObject);
+
+                EnemyList.Remove(nearestEnemy);
+            }
+
+
+            //Wait for 2 seconds
+            yield return new WaitForSeconds(1);
+
+        }
+    }
+}
+
---
+++ Assets/Scripts/2-player/ShootTheEnemy.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof (ClosestEnemy))]
+public class ShootTheEnemy : MonoBehaviour
+{
+    private ClosestEnemy script;
+    private Transform enemy;
+    private float radius = 2f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        ClosestEnemy script = GetComponent<ClosestEnemy>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (script.nearestEnemy != null) {
+            enemy = script.nearestEnemy;
+
+            if (Vector2.Distance(transform.position, enemy.position) <= radius) {
+                Destroy(enemy);
+
+            }
+        }
+    }
+}
