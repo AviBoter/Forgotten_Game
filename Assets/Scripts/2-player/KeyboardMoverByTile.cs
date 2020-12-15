4fa27db14305b42b25f01a33c9601d94046fd480
Benitk <31191253+Benitk@users.noreply.github.com>
2020-12-15 08:48:04 -0800

    finished game

--- Assets/Scripts/2-player/KeyboardMoverByTile.cs
+++ Assets/Scripts/2-player/KeyboardMoverByTile.cs
@@ -1,33 +1,67 @@
-﻿using UnityEngine;
+﻿using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
 using UnityEngine.Tilemaps;
+using static System.Collections.Generic.Dictionary<UnityEngine.Vector3, UnityEngine.Vector3>;
 
 /**
  * This component allows the player to move by clicking the arrow keys,
  * but only if the new position is on an allowed tile.
  */
-public class KeyboardMoverByTile: KeyboardMover {
+public class KeyboardMoverByTile : KeyboardMover
+{
     [SerializeField] Tilemap tilemap = null;
     [SerializeField] AllowedTiles allowedTiles = null;
     [SerializeField] TileBase newTile = null;
     [SerializeField] TileBase oldTile = null;
+    [SerializeField] TileBase PlayerHome = null;
+    [SerializeField] TileBase HotAirBalloon = null;
+    private Dictionary<Vector3, Vector3> HotAirBalloons;
+    private float eps = 1f;
 
-    private TileBase TileOnPosition(Vector3 worldPosition) {
+    private void Start()
+    {
+        HotAirBalloons = new Dictionary<Vector3, Vector3>();
+        HotAirBalloons.Add(new Vector3(2.5f, -9.15f, 0f), new Vector3(11.59f, 7.56f, 0f));
+    }
+    private TileBase TileOnPosition(Vector3 worldPosition)
+    {
         Vector3Int cellPosition = tilemap.WorldToCell(worldPosition);
         return tilemap.GetTile(cellPosition);
     }
 
-    void Update()  {
+    void Update()
+    {
         Vector3 newPosition = NewPosition();
         TileBase tileOnNewPosition = TileOnPosition(newPosition);
 
-        if (allowedTiles.Contain(tileOnNewPosition)) {
+        if (allowedTiles.Contain(tileOnNewPosition))
+        {
             transform.position = newPosition;
-        } 
-        else if(!allowedTiles.Contain(tileOnNewPosition) && tileOnNewPosition.Equals(oldTile) && Input.GetKey(KeyCode.Space))
+        }
+        else if (!allowedTiles.Contain(tileOnNewPosition) && tileOnNewPosition.Equals(oldTile) && Input.GetKey(KeyCode.Space))
         {
             tilemap.SetTile(tilemap.WorldToCell(newPosition), newTile);
-        } 
-        else 
+        }
+        else if (tileOnNewPosition.Equals(PlayerHome) && Input.GetKey(KeyCode.Space))
+        {
+            Debug.Log("Home Sweet Home " + tileOnNewPosition + "!");
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+
+        }
+        else if (tileOnNewPosition.Equals(HotAirBalloon))
+        {
+            KeyCollection col = HotAirBalloons.Keys;
+            foreach (Vector3 v in col)
+            {
+                if (v.sqrMagnitude - newPosition.sqrMagnitude <= eps && Input.GetKey(KeyCode.Space))
+                {
+                    Debug.Log("Fly away " + tileOnNewPosition + "!");
+                    transform.position = HotAirBalloons[v];
+                }
+            }
+        }
+        else
         {
             Debug.Log("You cannot walk on " + tileOnNewPosition + "!");
         }
